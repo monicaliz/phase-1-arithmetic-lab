576e14016b947397b69bac0a87816d7808569926
monicaliz <monicaeliz3@gmail.com>
2022-05-02 16:56:29 -0400

    commit tree c49669389e6d87bc167ee0ac825ae3e1732ed142

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+//multiply
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+//random
+const random = Math.floor(Math.random() + 1)
+//mod
+const num3 = 8
+const num4 = 2
+const mod = num3 / num4
+//max
+const max = Math.max(10, 10, 20)
